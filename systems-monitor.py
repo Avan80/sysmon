567b88582361c00294fdd08567509cfda4af96cf
Avan80 <mdab7253@gmail.com>
2026-06-26 03:27:58 +0300

    refactor: switch output to JSON, removed print section

--- systems-monitor.py
+++ systems-monitor.py
@@ -1,37 +1,38 @@
 import psutil
+import json
 
 cpu_percent = psutil.cpu_percent(interval=1)
 cpu_threads = psutil.cpu_count()
 load_average = psutil.getloadavg()
-
 free_mem = psutil.virtual_memory().percent
 available_mem = psutil.virtual_memory().available
-
 io_stats = psutil.net_io_counters()
 
-print("CPU")
-
-print(f"  {'CPU usage percentage':<20} = {cpu_percent}%")
-print(f"  {'CPU threads':<20} = {cpu_threads}")
-print(f"  {'Load average':<20} = 1m {load_average[0]} / 5m {load_average[1]} / 15m {load_average[2]}")
-
-print("Memory")
-
-print(f"  {'Free':<20} = {free_mem}%")
-print(f"  {'Available':<20} = {available_mem / 1024**3:.2f} GB")
-
-print("Disk")
-
+disk = {}
 for partition in psutil.disk_partitions():
     try:
         usage = psutil.disk_usage(partition.mountpoint)
-        print(f"  {partition.mountpoint:<20} = {usage.percent}%")
+        disk[partition.mountpoint] = usage.percent
     except PermissionError:
         pass
 
-print("Network")
+metrics = {
+    "CPU": {
+        "CPU usage percentage": cpu_percent,
+        "CPU threads": cpu_threads,
+        "Load average": load_average,
+    },
+    "Memory": {
+        "Free": free_mem,
+        "Available": round(available_mem / 1024**3, 2),
+    },
+    "Disk": disk,
+    "Network": {
+        "Bytes sent": round(io_stats.bytes_sent / 1024**2, 2),
+        "Bytes received": round(io_stats.bytes_recv / 1024**2, 2),
+        "Packets sent": io_stats.packets_sent,
+        "Packets received": io_stats.packets_recv,
+    }
+}
 
-print(f"  {'Bytes sent':<20} = {io_stats.bytes_sent / 1024**2:.2f} MB")
-print(f"  {'Bytes received':<20} = {io_stats.bytes_recv / 1024**2:.2f} MB")
-print(f"  {'Packets sent':<20} = {io_stats.packets_sent}")
-print(f"  {'Packets received':<20} = {io_stats.packets_recv}")
+print(json.dumps(metrics, indent=2))
