6ec65e565f85ce19bf0dc8977997456db506487a
Avan80 <mdab7253@gmail.com>
2026-07-08 21:58:05 +0300

    Add Terraform config: IAM role, security group, EC2 instance for sysmon deployment

---
+++ terraform/main.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "sysmon-test"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_iam_role" "sysmon_iam_role" {
+  name = "sysmon-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "sysmon_iam_role_policy" {
+  name = "sysmon-ec2-role-policy"
+  role = aws_iam_role.sysmon_iam_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+          "logs:DescribeLogGroups",
+        ]
+        Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "sysmon_profile" {
+  name = "sysmon-instance-profile"
+  role = aws_iam_role.sysmon_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.sysmon_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_security_group" "sysmon_sg" {
+  name = "sysmon-sg"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["102.184.54.251/32"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "sysmon_instance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = "sysmon-test"
+  vpc_security_group_ids = [aws_security_group.sysmon_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.sysmon_profile.name
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "sysmon-test"
+    role = "sysmon"
+  }
+}
