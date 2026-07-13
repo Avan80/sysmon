0b15e3afedffb0cf03a5a85d0fab1211e8372e47
Avan80 <mdab7253@gmail.com>
2026-07-13 03:27:09 +0300

    Formatted main.tf correctly

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,7 @@ resource "aws_instance" "sysmon_instance" {
 
   user_data = file("user_data.sh")
 
-  
+
 
   tags = {
     Name = "sysmon-test"
@@ -128,5 +128,5 @@ resource "aws_instance" "sysmon_instance" {
 
 output "instance_public_ip" {
   value = aws_instance.sysmon_instance.public_ip
-  }
+}
 
